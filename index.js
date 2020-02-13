bd213c084a4dd519c028eb5a39588a13b6f046cc
benhayehudi <bengreenberg@gmail.com>
2020-02-13 07:21:58 +0200

    add comments for desired behavior to write out

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const devurl = "https://dev.to/api/articles/me?page=1&per_page=6"
 let devPosts = [];
 
 // Get Latest DEV Posts
-let headers = {
-  "Content-Type": "application/json",
-  "api-key": `${process.env.DEV_API_KEY}`
-}
-
+// let headers = {
+//   "Content-Type": "application/json",
+//   "api-key": `${process.env.DEV_API_KEY}`
+// }
 // const getData = async devurl => {
 //   try {
 //     const response = await fetch(devurl, { method: 'GET', headers: headers});
@@ -25,12 +24,14 @@ let headers = {
 // devPosts = getData(devurl);
 // console.log(devPosts);
 
+// Get date and title of latest blog post
+
 // Run your GitHub Action!
 Toolkit.run(async tools => {
     // Print out the context in Actions dashboard
     console.log(tools.context);
 
-    // Assign data to variables
+    // Assign owner and repo data to variables
     const owner = tools.context.payload.repository.owner.login;
     const repo = tools.context.payload.repository.name;
     console.log(`THIS IS THE OWNER: ${owner} AND THIS IS THE REPO: ${repo}`);
@@ -42,4 +43,27 @@ Toolkit.run(async tools => {
       path: '_posts'
     })).data;
     console.log(`HERE ARE THE POSTS: ${posts}`);
+
+    // Count the number of posts in repo posts folder 
+
+    // Get the date and title of latest blog post in repo
+
+    // Get the path to the last blog post in repo
+
+    // Check to see if the repo posts folder has less posts than ${NUM_OF_POSTS} and count of DEV posts
+
+    // If yes, raise a PR that adds x - ${COUNT OF POSTS IN REPO} num of DEV posts to repo as markdown files
+
+    // If not, do nothing
+
+    // Check to see if the latest DEV post is newer than the latest repo post
+
+    // If yes, raise a PR that deletes the last blog post, 
+    // adds a markdown file for the newest DEV post,
+    // and increments the post num for the remaining posts by 1
+    // (i.e. 2020-02-03-post-3 becomes 2020-02-03-post-4)
+
+    // If not, do nothing
+
+
 });
