d3e2e1f86bf8895a2f071e2a31d367af274b4a0f
benhayehudi <bengreenberg@gmail.com>
2020-02-17 19:37:41 +0200

    another attempt

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const getData = () => {
     //   return devPosts;
     // });
 };
-const devPosts = await getData();
+const devPosts = async function(){await getData()};
 
 console.log(`OUTSIDE THE GET REQUEST: \n\n ${devPosts}`);
 // Get date and title of latest blog post
